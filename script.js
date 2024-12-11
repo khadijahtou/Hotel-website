a3e9efec66ec3556ff2b4d1e218344c01df2dd25
khadijahtou <khadijahtouu@gmail.com>
2024-12-11 12:54:01 +0100

    add javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+javascript
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const closeMenu = document.getElementById('closeMenu');
+    const navMenu = document.getElementById('navMenu');
+
+    if (menuToggle && navMenu && closeMenu) {
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+
+        closeMenu.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    }
+});
