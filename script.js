670cd776aab181f4f2fcaeb4fc37e5b14d2f6190
khadijahtou <khadijahtouu@gmail.com>
2024-12-16 19:41:45 +0100

    style the nav section and hero section

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-javascript
 document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const closeMenu = document.getElementById('closeMenu');
-    const navMenu = document.getElementById('navMenu');
+    const menuToggle = document.getElementById('open-menu');
+    const closeMenu = document.getElementById('close-menu');
+    const navMenu = document.getElementById('mobile-menu');
 
     if (menuToggle && navMenu && closeMenu) {
         menuToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
+            navMenu.classList.toggle('hidden')
+			closeMenu.classList.toggle('hidden')
+			menuToggle.classList.toggle('hidden')
         });
 
         closeMenu.addEventListener('click', () => {
-            navMenu.classList.remove('active');
+            navMenu.classList.toggle('hidden')
+			closeMenu.classList.toggle('hidden')
+			menuToggle.classList.toggle('hidden');
         });
     }
 });
